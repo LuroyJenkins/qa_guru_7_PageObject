1bc3571d0da11d72daec16d0e07f3e6c0a845d66
Nikita Starkov <starkfinity@gmail.com>
2023-06-04 12:33:46 +0500

    Add condition for dayCSSselector.

--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -2,19 +2,16 @@ package pages.components;
 
 import com.codeborne.selenide.SelenideElement;
 
-import java.time.Month;
-
 import static com.codeborne.selenide.Selectors.byText;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CalendarComponent {
     SelenideElement monthSelector = $(".react-datepicker__month-select"),
             yearSelector = $(".react-datepicker__year-select"),
-            daySelector = $(".react-datepicker__month");
+            daySelector = $(".react-datepicker__month:not(.react-datepicker__day--outside-month");
 
     public void setDateOfBirth(String day, String month, String year) {
-        int monthInt = Month.valueOf(month.toUpperCase()).getValue();
-        monthSelector.selectOption(monthInt - 1);
+        monthSelector.selectOption(month);
         yearSelector.selectOption(year);
         daySelector.$(byText(day)).click();
     }
